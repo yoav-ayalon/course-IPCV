155d3ec03406529c451d43e735f40f9c1e314714
Reif Keren <reifk@post.bgu.ac.il>
2025-12-07 10:36:19 +0200

    added binary mask, cleahe, unsharpen mask, custum kernel, sobel, laplacian, canny

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 # import scipy.stats as stats
 import cv2
 # from skimage import exposure
+from skimage import filters, exposure
 import os
 from skimage.io import imread, imsave
 from skimage.color import rgb2hsv, hsv2rgb
 # from skimage.util import img_as_float
 from typing import List, Union
 from pathlib import Path
+import scipy.ndimage as ndi
+#scikit-image==0.24.0
+#opencv-python
 
 
 script_dir = Path(__file__).resolve().parent
@@ -117,7 +121,7 @@ def orb_features(img_bgr):
     return rgb, keypoints, descriptors
 
 
-def select_roi_on_rgb(rgb, max_width=1200, max_height=1000):
+def select_roi_on_rgb(rgb, max_width=1200, max_height=800):
     """
     Allow user to select a rectangular ROI on the RGB image using mouse.
     Returns (rect_coords, roi_rgb) where rect_coords = (x1, y1, x2, y2).
@@ -216,13 +220,117 @@ def create_blurred_mask(rgb, global_points):
     blurred_full = cv2.GaussianBlur(rgb, ksize=(0, 0), sigmaX=25, sigmaY=25)
     return mask, blurred_full
 
+def mean_blur(gray, rgb, global_points):
+    h, w = rgb.shape[:2]
+    mask = np.zeros((h, w), dtype=np.uint8)
 
+    pts = np.array(global_points, dtype=np.int32)
+    hull = cv2.convexHull(pts)
+    cv2.fillConvexPoly(mask, hull, 255)
 
+    # 31x31 mean blur (strong blur, must be odd and > 1)
+    blurred_full = cv2.boxFilter(rgb, ddepth=-1, ksize=(31, 31), normalize=True)
 
+    return mask, blurred_full
 
+def binary_mask(roi_gray):
+    t_otsu = filters.threshold_otsu(roi_gray)
+    roi_binary_mask = (roi_gray > t_otsu).astype(np.uint8) * 255
+    #show_image([roi_gray, roi_binary_mask], row_plot=1)
+    # roi_binary_mask: 0/255 uint8 mask from above
+    return cv2.bitwise_and(roi_gray, roi_gray, mask=roi_binary_mask)
 
+def _plot_histogram(ax, image, alpha=0.3, **kwargs):
+    hist, bin_centers = exposure.histogram(image)
+    ax.fill_between(bin_centers, hist, alpha=alpha, **kwargs)
+    ax.set_xlabel('intensity')
+    ax.set_ylabel('# pixels')
 
+def iter_channels(color_image):
+    for channel in np.rollaxis(color_image, -1):
+        yield channel
 
+def plot_histogram(image, ax=None, **kwargs):
+    ax = ax if ax is not None else plt.gca()
+    if image.ndim == 2:
+        _plot_histogram(ax, image, color='black', **kwargs)
+    elif image.ndim == 3:
+        for channel, channel_color in zip(iter_channels(image), 'rgb'):
+            _plot_histogram(ax, channel, color=channel_color, **kwargs)
+
+def match_axes_height(ax_src, ax_dst):
+    plt.draw()
+    dst = ax_dst.get_position()
+    src = ax_src.get_position()
+    ax_dst.set_position([dst.xmin, src.ymin, dst.width, src.height])
+
+def imshow_with_histogram(image, **kwargs):
+    """
+    Show image and its histogram side-by-side.
+    """
+    width, height = plt.rcParams['figure.figsize']
+    fig, (ax_image, ax_hist) = plt.subplots(ncols=2, figsize=(2 * width, height))
+    kwargs.setdefault('cmap', plt.cm.gray)
+    ax_image.imshow(image, **kwargs)
+    plot_histogram(image, ax=ax_hist)
+    ax_image.set_axis_off()
+    match_axes_height(ax_image, ax_hist)
+    return ax_image, ax_hist
+
+def clahe(roi_gray):
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    he_clahe = clahe.apply(roi_gray)
+    ax_img, ax_hist = imshow_with_histogram(he_clahe)
+    show_image([roi_gray, he_clahe], row_plot=1, titles=["Original", "CLAHE (local equalization)"],)
+    return he_clahe
+
+def unsharp_masking(roi_gray):
+    blur = ndi.gaussian_filter(roi_gray, sigma=1.0)
+    # High-frequency component (edges/details)
+    high_freq = roi_gray - blur
+    # Sharpen: original + α * high_freq
+    alpha = 0.5
+    sharp = np.clip(roi_gray + alpha * high_freq, 0, 1)
+    show_image([roi_gray, blur, high_freq, sharp], row_plot=1, titles=["Original", "Blurred", "High freq (a - blur)", "Sharpened (unsharp mask)"])
+    return high_freq
+
+def custom_kernel(roi_gray):
+    kernel_sharp = np.array([[-1,-1,-1],
+                         [-1, 9,-1],
+                         [-1,-1,-1]], np.float32)
+    sharp = cv2.filter2D(roi_gray, -1, kernel_sharp)
+    show_image([sharp], titles=["sharp"], row_plot=1)
+    return sharp
+
+def sobel(roi_gray):
+    # Sobel gradients (derivatives)
+    sx = cv2.Sobel(roi_gray, cv2.CV_32F, 1, 0, ksize=3)
+    sy = cv2.Sobel(roi_gray, cv2.CV_32F, 0, 1, ksize=3)
+    sobel_mag = cv2.magnitude(sx, sy)
+
+    show_image([sx, sy, sobel_mag], row_plot=1,
+           titles='Sobel X | Sobel Y | Magnitude')
+    
+    return sobel_mag
+
+def laplacian(roi_gray):
+    # Laplacian (second derivative)
+    lap = cv2.Laplacian(roi_gray, cv2.CV_32F, ksize=3)
+
+    show_image([lap], row_plot=1,
+           titles='Laplacian')
+    
+    return lap
+
+def canny(roi_gray):
+    # Canny (works on 8-bit)
+    g8 = (roi_gray*255).astype(np.uint8)
+    edges = cv2.Canny(g8, threshold1=60, threshold2=180)
+
+    show_image([edges], row_plot=1,
+           titles='Canny')
+
+    return edges
 
 
 if __name__ == "__main__":
@@ -236,19 +344,46 @@ if __name__ == "__main__":
         show_image(roi_rgb, titles=["Selected ROI"], row_plot=1)
     
         roi_gray = cv2.cvtColor(roi_rgb, cv2.COLOR_RGB2GRAY)
-        corners = detect_corners_ShiTomasi(roi_gray, max_corner=20)
+
+        # Binary than grey mask
+        # roi_gray_masked = binary_mask(roi_gray)
+        # corners = detect_corners_ShiTomasi(roi_gray_masked, max_corner=30)
+
+        # Apply CLAHE on grayscale image
+        # he_clahe = clahe(roi_gray)
+        # corners = detect_corners_ShiTomasi(he_clahe, max_corner=30)
+
+        # Unsharp masking (blur → subtract → sharpen)
+        sharpen = unsharp_masking(roi_gray)
+        corners = detect_corners_ShiTomasi(sharpen, max_corner=30)
+
+        # sharp = custom_kernel(roi_gray)
+        # corners = detect_corners_ShiTomasi(sharp, max_corner=30)
+
+        # Sobel gradients (derivatives)
+        # sobel_mag = sobel(roi_gray)
+        # corners = detect_corners_ShiTomasi(sobel_mag, max_corner=30)
+
+        # Laplacian (second derivative)
+        # lap = laplacian(roi_gray)
+        # corners = detect_corners_ShiTomasi(lap, max_corner=30)
+
+        # Canny (works on 8-bit)
+        # edges = canny(roi_gray)
+        # corners = detect_corners_ShiTomasi(edges, max_corner=30)
+
+
+        # corners = detect_corners_ShiTomasi(roi_gray, max_corner=30)
         global_points = translate_corners_to_global(corners, rect_x1, rect_y1)
         
         rgb_debug = debug_draw_corners(rgb.copy(), global_points)
         show_image([rgb_debug], titles=["Corners debug"], row_plot=1)
 
         mask, blurred_full = create_blurred_mask(rgb, global_points)
+        #mask, blurred_full = mean_blur(gray, rgb, global_points)
+
         result = rgb.copy()
         result[mask == 255] = blurred_full[mask == 255]
         show_image([result], titles=["Object blurred"], row_plot=1)
 
-
-
-
-    
 
